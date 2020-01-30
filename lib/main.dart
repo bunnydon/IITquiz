145081bb3a6f283f0f57683b7a4ef2394c5e95a9
suchan khadka <suchan27khadka@gmail.com>
2020-01-30 12:51:02 +0545

    model and controller

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/Question.dart';
+import 'package:quizapp/Question_brain.dart';
+
+QuestionBrain questionBrain = new QuestionBrain();
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+
+  List<bool> answers = [true, false, true];
+
+  int questionNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionBrain.getQuestionText(questionNumber),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                "True",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (questionBrain.getAnswerResult(questionNumber) == true) {
+                    scorekeeper.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                      size: 24,
+                    ));
+                  } else {
+                    scorekeeper.add(Icon(
+                      Icons.close,
+                      color: Colors.red,
+                      size: 24,
+                    ));
+                  }
+                  questionNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                "False",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (questionBrain.getAnswerResult(questionNumber) == false) {
+                    scorekeeper.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                      size: 24,
+                    ));
+                  } else {
+                    scorekeeper.add(Icon(
+                      Icons.close,
+                      color: Colors.red,
+                      size: 24,
+                    ));
+                  }
+                  questionNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scorekeeper,
+        )
+      ],
+    );
+  }
+}
